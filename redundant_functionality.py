df031e24af63b142027e0af4f2867c9602352647
MaxLevianSterling <98947104+MaxLevianSterling@users.noreply.github.com>
2022-03-13 10:31:19 +0100

    Delete redundant_functionality.py

--- redundant_functionality.py
+++
@@ -1,6 +0,0 @@
-# Get image coordinates when image is embedded in black
-true_points = np.argwhere(values[0])
-bottom_right = true_points.max(axis=0)
-
-# Original encoding act_fn
-act_fn = nn.LeakyReLU(0.2, inplace=True)
